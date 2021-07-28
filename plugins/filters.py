68cce07cb22e242066625f7b5524584eb31655b0
teja9949 <67798221+teja9949@users.noreply.github.com>
2021-07-28 09:51:18 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -7,6 +7,11 @@ import re
 import pyrogram
 import urllib.request
 import imdb
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import json
+import imdb
+import os
 
 
 from pyrogram import (
@@ -25,8 +30,10 @@ from bot import Bot
 from script import script
 from config import MAINCHANNEL_ID
 
-def error(update, context):
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
+ia = imdb.IMDb() 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def reply(update, context):
