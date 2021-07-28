50b0e2094257e0b470820994ed0db3ad442fb58a
teja9949 <67798221+teja9949@users.noreply.github.com>
2021-07-28 10:34:10 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -78,7 +78,10 @@ def main():
 
         
         dp.add_handler(MessageHandler(Filters.text, reply))
+        dp.add_error_handler(error)
 
+        updater.start_polling() 
+        updater.idle()
 
 if __name__ == '__main__': 
      main()
