20705f3cbfd5b3bbbb84349fab50762bb39159f9
teja9949 <67798221+teja9949@users.noreply.github.com>
2021-07-28 11:32:19 +0530

    Update script.py

--- script.py
+++ script.py
@@ -5,26 +5,26 @@ class script(object):
 
 You can call this as an Auto Filter Bot if you like :D
 
-This bot is exclusively made for @dcompanymoviess
+This bot Is Made Only For @TeleXMovies
 
-You can see me in @dcompanymoviess <i>help</i></b>"""
+Join Our Movies Group @TeleXMovies <i>help</i></b>"""
 
 
     HELP_MSG = """<b>How to use the bot??</b>
-<i>    Sorry you can't use me 😔,for movies join @trolldcompany
+<i>  This Bot Is No Use For You\n This Bot is Specially Designed for @TeleXMovies
 </i>
 
-<b>© @Trolldcompany</b>"""
+<b>© @TeleXMoviesChannel\n @TeleXMovies\n @TeleXSeries</b>"""
 
 
-    ABOUT_MSG = """⭕️<b>My Name : D C Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : SAM 😊</b>
     
-⭕️<b>Creater :</b> @n_a_c_bot_developers
+⭕️<b>Creater :</b> @Teja_9949
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
-⭕️<b>support group :</b> <a href='https://t.me/botsfor_you_bot'>click here</a> 
+⭕️<b>Join Our Group :</b> <a href='https://t.me/teleXmovies'>click here</a> 
 
 """
