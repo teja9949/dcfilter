e8a98107568ad07c40484b1150d2b66011444f17
teja9949 <67798221+teja9949@users.noreply.github.com>
2021-07-28 09:29:21 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -84,7 +84,7 @@ async def filter(client: Bot, message: Message):
                 [InlineKeyboardButton(text="📃 Pages 1/1",callback_data="pages")]
             )
             await message.reply_text(
-                f"{ans}<b> Here is the result for {message.text}</b>",
+                f"{update.message}<b> Here is the result for {message.text}</b>",
                 reply_markup=InlineKeyboardMarkup(buttons)
             )
 
@@ -99,7 +99,7 @@ async def filter(client: Bot, message: Message):
         )
 
         await message.reply_text(
-                f"{ans}<b> Here is the result for {message.text}</b>",
+                f"{update.message}<b> Here is the result for {message.text}</b>",
                 reply_markup=InlineKeyboardMarkup(buttons)
             )    
 
